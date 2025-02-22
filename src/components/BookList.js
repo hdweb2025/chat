0ba54fc6a78deedaad2021302f3079c35c9eaa3c
hdweb2025 <hd.website19@gmail.com>
2025-02-22 23:13:51 +0700

    Update BookList.js

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,1 +1,24 @@
+import React from "react";
+
+const BookList = ({ setSelectedBook }) => {
+  const books = ["Judul Buku 1", "Judul Buku 2", "Judul Buku 3", "Judul Buku 4"];
+
+  return (
+    <div className="mt-4">
+      <label className="block text-sm">Daftar Buku yang tersedia</label>
+      <select
+        className="mt-2 p-2 border w-full"
+        onChange={(e) => setSelectedBook(e.target.value)}
+      >
+        {books.map((book, index) => (
+          <option key={index} value={book}>
+            {book}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default BookList;
 
