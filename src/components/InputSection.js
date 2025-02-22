64a966fd85d408deebd32a3ed55941c86b8d893c
hdweb2025 <hd.website19@gmail.com>
2025-02-22 23:12:44 +0700

    Update InputSection.js

--- src/components/InputSection.js
+++ src/components/InputSection.js
@@ -1,1 +1,17 @@
+import React from "react";
+
+const InputSection = ({ setSummary }) => {
+  return (
+    <div className="mt-4">
+      <textarea
+        className="w-full p-2 border rounded"
+        placeholder="Silahkan meringkas buku maksimal 300 kata"
+        onChange={(e) => setSummary(e.target.value)}
+      ></textarea>
+      <button className="bg-purple-600 text-white py-2 px-4 w-full mt-2">Submit</button>
+    </div>
+  );
+};
+
+export default InputSection;
 
