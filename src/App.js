af79f61696d3908eeeb39d17349a03e8566657cb
hdweb2025 <hd.website19@gmail.com>
2025-02-22 23:10:28 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,17 +26,6 @@ const App = () => {
 
 export default App;
 
-// UploadSection.js
-export const UploadSection = () => {
-  return (
-    <div className="mt-4">
-      <label className="block text-sm">Upload buku yang akan di review</label>
-      <input type="file" className="mt-2 p-2 border w-full" />
-      <button className="bg-purple-600 text-white py-2 px-4 w-full mt-2">Upload</button>
-    </div>
-  );
-};
-
 // BookList.js
 export const BookList = ({ setSelectedBook }) => {
   const books = ["judul Buku", "judul Buku", "judul Buku", "judul Buku"];
