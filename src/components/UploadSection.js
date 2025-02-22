1bd419ec6fd40b59bccabd3c341ea3b9e5e15426
hdweb2025 <hd.website19@gmail.com>
2025-02-22 23:11:08 +0700

    Update UploadSection.js

--- src/components/UploadSection.js
+++ src/components/UploadSection.js
@@ -1,1 +1,12 @@
+const UploadSection = () => {
+  return (
+    <div className="mt-4">
+      <label className="block text-sm">Upload buku yang akan di review</label>
+      <input type="file" className="mt-2 p-2 border w-full" />
+      <button className="bg-purple-600 text-white py-2 px-4 w-full mt-2">Upload</button>
+    </div>
+  );
+};
+
+export default UploadSection;
 
